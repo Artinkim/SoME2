d3e1cb88ad6fcecffadc78f4a82a60867e41a6cb
Spacefroyo <44252902+Spacefroyo@users.noreply.github.com>
2022-08-04 11:23:05 -0700

    Update 1.4.py

--- manim/1.4.py
+++ manim/1.4.py
@@ -36,15 +36,15 @@ class RandomMatrix(Scene):
         m.append(MobjectMatrix([[MathTex(m[-1][0][size[1]*x+y].tex_string+np.random.choice(signs)+str(np.random.randint(low=bounds[0], high=bounds[1]))+"j"+np.random.choice(signs)+str(np.random.randint(low=bounds[0], high=bounds[1]))+"k") for y in range(size[1])] for x in range(size[0])], h_buff = 4.6))
         m.append(MobjectMatrix([[MathTex(m[-1][0][size[1]*x+y].tex_string+np.random.choice(signs)+"...") for y in range(size[1])] for x in range(size[0])], h_buff = 5.4))
         m.append(MobjectMatrix([
-            [MathTex("M_11"), MathTex("M_12"), MathTex("..."), MathTex("M_1P")],
-            [MathTex("M_21"), MathTex("M_22"), MathTex("..."), MathTex("M_1P")],
+            [MathTex("M_{11}"), MathTex("M_{12}"), MathTex("..."), MathTex("M_{1P}")],
+            [MathTex("M_{21}"), MathTex("M_{22}"), MathTex("..."), MathTex("M_{2P}")],
             [MathTex(), MathTex(), MathTex("..."), MathTex()],
-            [MathTex("M_N1"), MathTex("M_N2"), MathTex("..."), MathTex("M_NP")]], h_buff = 1.6))
+            [MathTex("M_{N1}"), MathTex("M_{N2}"), MathTex("..."), MathTex("M_{NP}")]], h_buff = 1.6))
         m.append(MobjectMatrix([
-            [MathTex("M_11"), MathTex("M_12"), MathTex("..."), MathTex("M_1N")],
-            [MathTex("M_21"), MathTex("M_22"), MathTex("..."), MathTex("M_1N")],
+            [MathTex("M_{11}"), MathTex("M_{12}"), MathTex("..."), MathTex("M_{1N}")],
+            [MathTex("M_{21}"), MathTex("M_{22}"), MathTex("..."), MathTex("M_{2N}")],
             [MathTex(), MathTex(), MathTex("..."), MathTex()],
-            [MathTex("M_N1"), MathTex("M_N2"), MathTex("..."), MathTex("M_NN")]], h_buff = 1.6))
+            [MathTex("M_{N1}"), MathTex("M_{N2}"), MathTex("..."), MathTex("M_{NN}")]], h_buff = 1.6))
 
         size = (5, 5)
         bounds = (-10, 10)
@@ -83,4 +83,29 @@ class RandomMatrix(Scene):
 
         self.add(m[-1])
         self.play(vt.animate.set_value(1), run_time=5)
+        self.remove(m[-1])
+
+        m = []
+        for _ in range(3):
+            m.append(Matrix([[np.round(bounds[0]+(bounds[1]-bounds[0])*np.random.random(), decimals=2) for y in range(size[1])] for x in range(size[0])], h_buff = 1.6))
+            m[-1].scale(0.5)
+        
+        # self.add(g)
+        for i in range(3):
+            self.add(m[i])
+            # m[i].move_to(UP*1)
+            self.wait(1)
+            self.play(m[i].animate.scale(0.4).move_to(LEFT * 2*(1-i) + DOWN * 1.5))
+            
+        g = Group(m[0], m[1], m[2])
+        self.remove(m[0], m[1], m[2])
+        self.add(g)
+        self.wait(1)
+        a, b = MathTex("\\{"), MathTex("\\}")
+        a.scale(1.7)
+        b.scale(1.7)
+        ensemble = Group(MathTex("ensemble: "), a, m[0].copy(), m[1].copy(), MathTex("..."), m[2].copy(), b).arrange_in_grid(rows=1, cols=7)
+        self.play(TransformMatchingShapes(g, ensemble))
+        # self.add(ensemble)
+
         self.wait(5)
