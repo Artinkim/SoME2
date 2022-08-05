aa1bb2f5cc6249dfab45e508b3232943f5a3bcae
Spacefroyo <petergao322@gmail.com>
2022-08-05 12:36:09 -0700

    1.5.py p1

--- manim/1.5.py
+++ manim/1.5.py
@@ -49,20 +49,42 @@ from utils import ChangingMatrix
 
 class EnsembleAverage(Scene):
     def construct(self):
+
         size, bounds = (2, 2), (-10, 10)
         iterations = 100
         matricies = [np.round(bounds[0]+(bounds[1]-bounds[0])*np.random.rand(size[0], size[1]), decimals=2) for _ in range(iterations)]
         t = ValueTracker(0)
-        matrix = ChangingMatrix(matricies,t)
+        
+        test = -1
+        while test < 0:
+            matrix = ChangingMatrix(matricies,t)
+            test = matrix.nums[1][1].get_value()
+        numsV = [[matrix.nums[x][y].get_value() for y in range(len(matrix.nums[0]))] for x in range(len(matrix.nums))]
 
         matrixLabel = MathTex("M = ").next_to(matrix, direction=LEFT)
         self.add(matrix, matrixLabel)
+        nums = [[MathTex(str(matrix.nums[x][y].get_value())).move_to(matrix.nums[x][y]) for y in range(len(matrix.nums[0]))] for x in range(len(matrix.nums))]
         
-        matrix_21 = matrix.nums[1][0]
-        eq = MathTex("M_{21} = ").next_to(matrix, direction=DOWN)
-        self.add(eq)
-        self.play(matrix_21.copy().move_to(matrix_21).animate.next_to(eq))
+        entryEq = MathTex("M_{11} = ").next_to(matrix, direction=DOWN).shift(LEFT*2.5)
+        self.play(nums[0][0].animate.next_to(entryEq), FadeIn(entryEq, scale=1.5))
+        self.wait(1)
 
+        traceEq = MathTex("trace(M) = ").move_to(entryEq, aligned_edge=LEFT)
+        tracePlus = MathTex(" + ").next_to(nums[0][0].copy().next_to(traceEq))
+        self.play(nums[0][0].animate.next_to(traceEq), nums[1][1].animate.next_to(tracePlus), Transform(entryEq, traceEq), FadeIn(tracePlus, scale=1.5))
+        self.wait(0.5)
+        trace = MathTex(str(np.round(np.trace(numsV), decimals=2))).next_to(traceEq)
+        traceRight = Group(nums[0][0], tracePlus, nums[1][1])
+        self.play(Transform(traceRight, trace))
+        self.wait(1)
+        self.remove(entryEq)
 
+        detEq = MathTex("det(M) = ").move_to(entryEq, aligned_edge=LEFT)
+        nums = [[MathTex(str(matrix.nums[x][y].get_value())).move_to(matrix.nums[x][y]) for y in range(len(matrix.nums[0]))] for x in range(len(matrix.nums))]
+        detRight = MathTex("(", nums[0][0].get_tex_string(), "*", nums[1][1].get_tex_string(), ") - (", nums[0][1].get_tex_string(), "*", nums[1][0].get_tex_string(), ")").next_to(detEq)
+        self.play(FadeOut(traceRight, scale = 0.5), Transform(traceEq, detEq), TransformMatchingTex(Group(*nums[0], *nums[1]), detRight))
+        self.wait(0.5)
+        det = MathTex(str(np.round(np.linalg.det(numsV), decimals=2))).next_to(detEq)
+        self.play(Transform(detRight, det))
 
         self.wait(5)
