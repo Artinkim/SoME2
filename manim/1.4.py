49325b36f11af3f289a22c37a1e87352cccebfc8
ArtinKim <artin513@sbcglobal.net>
2022-08-05 21:09:05 -0700

    progress and refactoring 1.4

--- manim/1.4.py
+++ manim/1.4.py
@@ -1,7 +1,7 @@
 from manim import *
 import numpy as np
 
-class ChangingMatrix(MobjectMatrix):
+class RadomMatrix(MobjectMatrix):
     
     def __init__(self, size, bounds, precision, vt):
         self.size = size
@@ -10,10 +10,8 @@ class ChangingMatrix(MobjectMatrix):
         self.nums = [[DecimalNumber() for _ in range(self.size[1])] for __ in range(self.size[0])]
         for x in range(self.size[0]):
             for y in range(self.size[1]):
-                # self.nums[x][y].arrange(direction=RIGHT)
                 self.nums[x][y].add_updater(lambda d: d.set_value(np.round(vt.get_value()*0+self.bounds[0]+(self.bounds[1]-self.bounds[0])*np.random.random(), decimals=self.precision)))
 
-        # self.add_updater(lambda m: vt*0)
         super().__init__(self.nums, h_buff=2, element_alignment_corner=DR)
         
 
@@ -23,9 +21,10 @@ class RandomMatrix(Scene):
     def construct(self):
         np.random.seed(1)
         vt = ValueTracker(0)
+        
+        m = []
         size = (2, 2)
         bounds = (1, 10)
-        m = []
         m.append(MobjectMatrix([[DecimalNumber(np.round(np.random.uniform(*bounds), decimals=2)) for y in range(size[1])] for x in range(size[0])], h_buff = 1.6))
         m.append(MobjectMatrix([[MathTex("Gaussian(\mu,\sigma)") for y in range(size[1])] for x in range(size[0])], h_buff = 3.5))
         m.append(MobjectMatrix([[MathTex("Uniform (a,b)") for y in range(size[1])] for x in range(size[0])], h_buff = 3.5))
@@ -43,18 +42,19 @@ class RandomMatrix(Scene):
             [MathTex("M_{21}"), MathTex("M_{22}"), MathTex("..."), MathTex("M_{2N}")],
             [MathTex(), MathTex(), MathTex("..."), MathTex()],
             [MathTex("M_{N1}"), MathTex("M_{N2}"), MathTex("..."), MathTex("M_{NN}")]], h_buff = 1.6))
-        
         size = (5, 5)
         bounds = (-10, 10)
         m.append(MobjectMatrix([[DecimalNumber(np.round(np.random.uniform(*bounds), decimals=2)) for y in range(size[1])] for x in range(size[0])], h_buff = 1.6))
-        m.append(ChangingMatrix(size=size, bounds=bounds, precision=2, vt=vt))
+        m.append(RadomMatrix(size=size, bounds=bounds, precision=2, vt=vt))
         
+        # scale matricies then add first matrix to the scene
         for x in m:
             x.scale(0.5)
-        a = m[0]
+        a = m[0].copy()
         self.add(a)
         self.wait(1)
-
+        
+        # initialize axis and graphs for the two distributions
         axes1 = Axes(x_range=[-5,6,1], y_range=[0,1.1, 0.2]).scale(0.5).add_coordinates().scale(0.5).move_to(UL*2+LEFT*2)
         axes1.add(Tex("0").scale(0.4).next_to(axes1.x_axis.n2p(0), DOWN*0.5))
         guas = axes1.plot(lambda x: np.exp(-(x/3)**2),color=BLUE)
@@ -63,73 +63,89 @@ class RandomMatrix(Scene):
         uniform = axes2.plot(lambda x: 1 if x>-3 and x<3 else 0, discontinuities=[-3,3], color=GREEN)
         uniform_area = axes2.get_area(uniform)
 
+        # play chaning the matrix and showing distributions
         self.play(Transform(a, m[1]),Create(axes1),Create(guas),run_time=1)
         self.play(FadeIn(guas_area))
         self.wait(1)
-        self.play(Transform(a, m[2]),ReplacementTransform(axes1,axes2,rate_function=lingering),Create(uniform,rate_func=rush_into),run_time=1)
+        self.play(Transform(a, m[2]),Transform(axes1,axes2,rate_function=lingering),Create(uniform,rate_func=rush_into),run_time=1)
         self.play(FadeIn(uniform_area))
         self.wait(1)
         
+        # create list of element types matrix can hold
         element_types = VGroup(
             MathTex("a"),
             MathTex("a + bi"),
             MathTex("{\displaystyle a+b\ \mathbf {i} +c\ \mathbf {j} +d\ \mathbf {k} }"),
             MathTex("{\displaystyle a+b\ \mathbf {i} +c\ \mathbf {j} +d\ \mathbf {k} }","...")
-        ).arrange(DOWN).scale(0.75).next_to(axes2)
-    
-        self.play(Transform(a, m[0]), FadeOut(axes2), FadeIn(element_types[0]))
+        ).arrange(DOWN, center=False, aligned_edge=LEFT).scale(0.75).next_to(axes1)
+
+        # play changing the matrix and showing element types
+        self.play(Transform(a, m[0]), FadeOut(axes1), FadeIn(element_types[0]))
+        self.wait(1)
         self.play(Transform(a, m[3]), FadeIn(element_types[1]))
         self.play(Transform(a, m[4]), FadeIn(element_types[2]))
         self.play(Transform(a, m[5]), FadeIn(element_types[3]))
-
         self.wait(1)
-        self.play(Transform(a, m[6]))
-        b = Brace(a)
-        b1tex, b2tex = b.get_tex("N\\text{x}P"), b.get_tex("N\\text{x}N")
-        self.add(b, b1tex)
+
+        # shows sizes of matrices 
+        b = Brace(m[6])
+        b1tex, b2tex = b.get_tex("N\\text{x}P"), b.get_tex("N\\text{x}N") 
+        self.play(Transform(a, m[6]),FadeIn(b,b1tex))
         self.wait(1)
         self.play(Transform(a, m[7]), Transform(b1tex, b2tex))
         self.wait(1)
-        self.remove(b, b1tex)
-        self.play(Transform(a, m[-2]))
-        # self.wait(0.5)
-        self.remove(a)
         
-        # self.add(a)
-        # self.add(b, b1tex)
-        # self.play(Transform(a, m[1]), Transform(b1tex, b2tex))
-        # self.wait(1)
-        # self.remove(b, b1tex)
-        # for i in range(1, len(m)-2):
-        #     self.play(Transform(a, m[i+1]))
-        # self.remove(a)
-
-        self.add(m[-1])
-        self.play(vt.animate.set_value(1), run_time=5)
-        self.remove(m[-1])
-
-        m = []
-        for _ in range(3):
-            m.append(Matrix([[np.round(bounds[0]+(bounds[1]-bounds[0])*np.random.random(), decimals=2) for y in range(size[1])] for x in range(size[0])], h_buff = 1.6))
-            m[-1].scale(0.5)
-        
-        # self.add(g)
-        for i in range(3):
-            self.add(m[i])
-            # m[i].move_to(UP*1)
-            self.wait(1)
-            self.play(m[i].animate.scale(0.4).move_to(LEFT * 2*(1-i) + DOWN * 1.5))
-            
-        g = Group(m[0], m[1], m[2])
-        self.remove(m[0], m[1], m[2])
-        self.add(g)
+        # show specific case of matrix 5x5 (-10,10)
+        axes3 = Axes(x_range=[-12,13,2], y_range=[0,1.1, 0.2]).scale(0.5).add_coordinates().scale(0.5).move_to(UL*2+LEFT)
+        axes3.add(Tex("0").scale(0.4).next_to(axes3.x_axis.n2p(0), DOWN*0.5))
+        uniformN = axes3.plot(lambda x: 1 if x>-10 and x<10 else 0, discontinuities=[-10,10], color=GREEN)
+        uniform_areaN = axes3.get_area(uniformN)
+        integer_tex = element_types.submobjects[0]
+        integer_texN = integer_tex.copy().next_to(axes3).shift(RIGHT)
+        element_types.remove(integer_tex)
+        self.play(
+            Transform(a, m[8]),
+            FadeOut(guas,guas_area,b,b1tex),
+            Transform(uniform,uniformN),
+            Transform(uniform_area,uniform_areaN),
+            FadeIn(axes3),
+            FadeOut(*element_types.submobjects),
+            Transform(integer_tex,integer_texN),
+        )
+        integer_arrow = CurvedArrow(integer_texN.get_center(),axes3.c2p(1.8,0.8),color=YELLOW)
+        self.play(Create(integer_arrow))
+        self.play(Create(MathTex("M_{ij} = ").scale(0.5).next_to(integer_texN,LEFT)))
         self.wait(1)
-        a, b = MathTex("\\{"), MathTex("\\}")
-        a.scale(1.7)
-        b.scale(1.7)
-        ensemble = Group(MathTex("ensemble: "), a, m[0].copy(), m[1].copy(), MathTex("..."), m[2].copy(), b).arrange_in_grid(rows=1, cols=7)
-        self.play(TransformMatchingShapes(g, ensemble))
-        # self.add(ensemble)
 
+        # cycles through random matricies, moving arrow along distribution
+        self.remove(a)
+        a = m[9].copy()
+        self.add(a)
+        curr_arrow = integer_arrow.copy()
+        # integer_arrow.add_updater(lambda m: m.position_tip(vt.get_value()*0+axes3.c2p(np.random.uniform(-10,10),np.random.uniform(-10,10))))
+        # self.play(vt.animate.set_value(1), run_time=1)
+        left_arrow = CurvedArrow(integer_texN.get_center(),axes3.c2p(-10,0.8),color=YELLOW)
+        right_arrow = CurvedArrow(integer_texN.get_center(),axes3.c2p(10,0.8),color=YELLOW)
+        self.play(vt.animate.set_value(0.3), Transform(curr_arrow,left_arrow), run_time=2)
+        self.play(vt.animate.set_value(0.6), Transform(curr_arrow,right_arrow), run_time=2)
+        self.play(vt.animate.set_value(1), Transform(curr_arrow,integer_arrow), run_time=2)
+
+        # show creation of 3 matricies then puts into row
+        self.remove(a) 
+        m_group = VGroup()
+        for i in range(3):
+            temp = Matrix(np.round(np.random.uniform(*bounds,size=size), decimals=2), h_buff = 1.6)
+            temp.scale(0.5)
+            m_group.add(temp)
+            self.add(temp)
+            self.wait(1)
+            self.play(m_group.submobjects[i].animate.scale(0.4).move_to(LEFT * 2*(1-i) + DOWN * 1.5))
+            self.wait(1)
+        
+        # show ensemble as collection of 3 previously created matricies
+        m = m_group.submobjects
+        a, b = MathTex("\\{").scale(1.7), MathTex("\\}").scale(1.7)
+        ensemble = Group(MathTex("ensemble: "), a, m[0].copy(), m[1].copy(), MathTex("..."), m[2].copy(), b).arrange(RIGHT)
+        self.play(TransformMatchingShapes(m_group, ensemble))
         self.wait(5)
 
