8281776e4f40aaa11280375c1d8c007c3db55a00
Artinkim <37283700+Artinkim@users.noreply.github.com>
2022-08-14 03:39:36 -0700

    glitches

--- manim/1.11.py
+++ manim/1.11.py
@@ -9,17 +9,23 @@ def CreateWave():
     return axes
 
     
-class Intro(Scene):
+class Intro1(Scene):
     def playWait(self, *args,**kwargs):
             self.play(*args,**kwargs)
             self.wait(1)
             
     def construct(self):
+        entry_matrix = MobjectMatrix([
+            [MathTex("M_{11}"), MathTex("M_{12}"), MathTex("..."), MathTex("M_{1P}")],
+            [MathTex("M_{21}"), MathTex("M_{22}"), MathTex("..."), MathTex("M_{2P}")],
+            [MathTex(), MathTex(), MathTex("\ddots"), MathTex()],
+            [MathTex("M_{N1}"), MathTex("M_{N2}"), MathTex("..."), MathTex("M_{NP}")]], h_buff = 1.6)
         unkown_matrix = MobjectMatrix([
             [MathTex("?"), MathTex("?"), MathTex("..."), MathTex("?")],
             [MathTex("?"), MathTex("?"), MathTex("..."), MathTex("?")],
             [MathTex(), MathTex(), MathTex("\ddots"), MathTex()],
             [MathTex("?"), MathTex("?"), MathTex("..."), MathTex("?")]], h_buff = 1.6)
+        
         waves1 = VGroup(*[CreateWave() for _ in range(7)]).arrange(DOWN)
         waves2 = VGroup(*[CreateWave() for _ in range(7)]).arrange(DOWN)
         schrodinger_equation1 = VGroup(MathTex("H"), MathTex("\psi_n"), MathTex(" &= "), MathTex("E_n"), MathTex("\psi_n")).arrange(RIGHT,buff=0.05)
@@ -27,9 +33,12 @@ class Intro(Scene):
         schrodinger_equation3 = VGroup(MathTex("H"), waves1, MathTex(" &= "), MathTex("E_n"),waves2).arrange(RIGHT,buff=0.05)
         schrodinger_equation4 = VGroup(MathTex("H").scale(2).set_color(RED), MathTex("\psi_n"), MathTex(" &= "), MathTex("E_n"), MathTex("\psi_n")).arrange(RIGHT,buff=0.05)
         schrodinger_equation5 = VGroup(MathTex("H").scale(2).set_color(RED), MathTex("\psi_n"), MathTex(" &= "), MathTex("E_n").scale(2).set_color(YELLOW), MathTex("\psi_n")).arrange(RIGHT,buff=0.05)
-        schrodinger_equation6 = VGroup(unkown_matrix, MathTex("\psi_n"), MathTex(" &= "), MathTex("E_n"), MathTex("\psi_n")).arrange(RIGHT,buff=0.05)
+        schrodinger_equation6 = VGroup(entry_matrix, MathTex("\psi_n"), MathTex(" &= "), MathTex("E_n"), MathTex("\psi_n")).arrange(RIGHT,buff=0.05)
+        schrodinger_equation7 = VGroup(entry_matrix, MathTex("\psi_n"), MathTex(" &= "), MathTex("(\\lambda_{1}, \\lambda_{2}, \\ldots)"), MathTex("\psi_n")).arrange(RIGHT,buff=0.05)
+        schrodinger_equation8 = VGroup(MathTex("H = "),unkown_matrix).arrange(RIGHT,buff=0.05).to_edge(LEFT,buff=1).shift(DOWN)
         curr_equation = schrodinger_equation1.copy()
         
+        eigen_list = MathTex("(\\lambda_{1}, \\lambda_{2}, \\ldots)").next_to(entry_matrix, DOWN)
         
         self.playWait(Write(curr_equation))
         self.playWait(Transform(curr_equation, schrodinger_equation2))
@@ -38,9 +47,77 @@ class Intro(Scene):
         self.playWait(Transform(curr_equation, schrodinger_equation4))
         self.playWait(Transform(curr_equation, schrodinger_equation5))
         self.playWait(Transform(curr_equation, schrodinger_equation6))
+        self.playWait(Write(eigen_list))
+        self.playWait(Transform(eigen_list,schrodinger_equation7.submobjects[3]),Transform(curr_equation, schrodinger_equation7))
+        self.remove(eigen_list)
+        self.playWait(Transform(curr_equation, schrodinger_equation1))
+        self.play(curr_equation.animate.to_edge(UL,buff=1))
         brace = Brace(unkown_matrix,UP)
         brace_tex = brace.get_text("? X ?")
-        self.playWait(Create(brace), Write(brace_tex))
+        self.playWait(ReplacementTransform(curr_equation.copy(), schrodinger_equation8),Create(brace), Write(brace_tex))
+
+
+# class Intro2(Scene):
+#     def playWait(self, *args,**kwargs):
+#             self.play(*args,**kwargs)
+#             self.wait(1)    
+               
+#     def construct(self):    
+#         max_entropy = MathTex("P(E_i) &= \\frac{e^{-\\beta E_i}}{Z};\>\> Z = \sum_i e^{-\\beta E_i}, \>\>").to_edge(UR,buff=1)
+       
+       
+#         exact_state = MathTex("(x_1,...,x_N ; p_1, ..., p_N)").shift(LEFT*3)
+#         weight_factor1 = MathTex("&\\rightarrow P(E) \propto e^{-E(x_1,...x_N; p_1,...,p_N) }").next_to(exact_state,RIGHT)
+        
+#         phase_state = MathTex("(\sigma_1,...,\sigma_N )").next_to(exact_state,DOWN)
+#         weight_factor2 = MathTex("&\\rightarrow P(E) \propto e^{-E(\sigma_1,...,\sigma_N)").next_to(phase_state,RIGHT)
+        
+#         energy_tex= MathTex("E({x1, ..., xN ; p1, ..., pN })")
+#         energy_state = MathTex(r"E &\rightarrow \langle E \rangle = \hspace{-.2cm} \sum_{\text{states of systems}} \hspace{-.2cm}E(\text{state}) P(E(\text{state}))\\ \text{exact energy} &\rightarrow \text{statistical prediction by averaging over states}")
+        
+        
+#         self.playWait(Write(max_entropy))
+        
+#         self.playWait(Write(exact_state))
+#         self.playWait(ReplacementTransform(exact_state.copy(),weight_factor1))
+        
+#         self.playWait(Write(phase_state))
+#         entry_matrix = MobjectMatrix([
+#             [MathTex("M_{11}"), MathTex("M_{12}"), MathTex("..."), MathTex("M_{1P}")],
+#             [MathTex("M_{21}"), MathTex("M_{22}"), MathTex("..."), MathTex("M_{2P}")],
+#             [MathTex(), MathTex(), MathTex("\ddots"), MathTex()],
+#             [MathTex("M_{N1}"), MathTex("M_{N2}"), MathTex("..."), MathTex("M_{NP}")]], h_buff = 1.6)
+#         unkown_matrix = MobjectMatrix([
+#             [MathTex("?"), MathTex("?"), MathTex("..."), MathTex("?")],
+#             [MathTex("?"), MathTex("?"), MathTex("..."), MathTex("?")],
+#             [MathTex(), MathTex(), MathTex("\ddots"), MathTex()],
+#             [MathTex("?"), MathTex("?"), MathTex("..."), MathTex("?")]], h_buff = 1.6)
+#         self.playWait(ReplacementTransform(unkown_matrix,RANDOM MATRIX))
+        
+#         self.playWait(
+#             Write(energy_tex),
+#             FadeOut(exact_state,weight_factor1,phase_state,weight_factor2,max_entropy)
+#         )
+#         self.playWait(ReplacementTransform(energy_tex,energy_state))
+        
+#         hamiltonian_weight = MathTex(r"H & \rightarrow P(M) \propto e^{-\frac{N}{2} Tr(HH^{\dagger})} \\ \text{exact system} & \rightarrow \text{weight factor for each system}\notag\\")
+#         self.playWait(Write(hamiltonian_weight))
+        
+#         self.playWait(ReplacementTransform())
+#         example1 = MathTex("x10 = 0.310231").next_to(energy_state,DOWN,buff=1).shift(LEFT)
+#         example2 = MathTex("s = 1.2").next_to(example1,RIGHT,buff=1)
+#         self.playWait(Write(example1))
+#         self.playWait(Write(example2))
+        
+#         tex_final = MathTex(r"(x_1,...,x_N) & \rightarrow \langle x_1\rangle, \>\> \langle x_1-x_2\rangle, ... \> \langle f(x_1,..,x_N)\rangle \\\text{exact spectrum} & \rightarrow \text{statistical properties of spectrum}\notag\\ \text{requires: knowing $\&$ diagonalizing $H$} & \rightarrow \>\> \rho(x), P(s)")
+#         tex_final.scale(0.75).shift(LEFT*1.5)
+#         self.playWait(FadeOut(energy_state,example1,example2))
+#         self.playWait(Write(tex_final))
+        
+        
+        
+#         # self.playWait(Write(hamiltonian_weight))
+        
         
         
         
--- manim/1.4.py
+++ manim/1.4.py
@@ -1,3 +1,4 @@
+from base64 import b16decode
 from functools import cache
 from manim import *
 import numpy as np
@@ -34,37 +35,53 @@ class RandomMatrix(Scene):
         vt = ValueTracker(0)
         
         m = []
-        size = (2, 2)
+        size = (2, 2) 
         bounds = (1, 10)
+        first2b2 = MobjectMatrix([
+            [MathTex("M_{11}"), MathTex("M_{12}")],
+            [MathTex("M_{21}"), MathTex("M_{22}")]], h_buff = 1.6)  
+        second2b2 = MobjectMatrix([
+            [MathTex("P(M_{11})"), MathTex("P(M_{12})")],
+            [MathTex("P(M_{21})"), MathTex("P(M_{22})")]], h_buff = 2)
         m.append(MobjectMatrix([[DecimalNumber(np.round(np.random.uniform(*bounds), decimals=2)) for y in range(size[1])] for x in range(size[0])], h_buff = 1.6))
-        m.append(MobjectMatrix([[MathTex("Gaussian(\mu,\sigma)") for y in range(size[1])] for x in range(size[0])], h_buff = 3.5))
-        m.append(MobjectMatrix([[MathTex("Uniform (a,b)") for y in range(size[1])] for x in range(size[0])], h_buff = 3.5))
+        m.append(MobjectMatrix([[MathTex("\\text{Gaussian} (\mu,\sigma^{2})") for y in range(size[1])] for x in range(size[0])], h_buff = 3.5))
+        m.append(MobjectMatrix([[MathTex("\\text{Uniform} (a,b)") for y in range(size[1])] for x in range(size[0])], h_buff = 3.5))
         signs = [" + ", " - "]
         m.append(MobjectMatrix([[MathTex(str(int(m[0][0][size[1]*x+y].get_value()))+np.random.choice(signs)+str(np.random.randint(low=bounds[0], high=bounds[1]))+"i") for y in range(size[1])] for x in range(size[0])], h_buff = 2.6))
         m.append(MobjectMatrix([[MathTex(m[-1][0][size[1]*x+y].tex_string+np.random.choice(signs)+str(np.random.randint(low=bounds[0], high=bounds[1]))+"j"+np.random.choice(signs)+str(np.random.randint(low=bounds[0], high=bounds[1]))+"k") for y in range(size[1])] for x in range(size[0])], h_buff = 4.6))
         m.append(MobjectMatrix([[MathTex(m[-1][0][size[1]*x+y].tex_string+np.random.choice(signs)+"...") for y in range(size[1])] for x in range(size[0])], h_buff = 5.4))
         m.append(MobjectMatrix([
-            [MathTex("M_{11}"), MathTex("M_{12}"), MathTex("..."), MathTex("M_{1P}")],
-            [MathTex("M_{21}"), MathTex("M_{22}"), MathTex("..."), MathTex("M_{2P}")],
-            [MathTex(), MathTex(), MathTex("\ddots"), MathTex()],
-            [MathTex("M_{N1}"), MathTex("M_{N2}"), MathTex("..."), MathTex("M_{NP}")]], h_buff = 1.6))
+            [MathTex("M_{11}"), MathTex("M_{12}"),MathTex("M_{13}"), MathTex("..."), MathTex("M_{1P}")],
+            [MathTex("M_{21}"), MathTex("M_{22}"), MathTex("M_{23}"), MathTex("..."), MathTex("M_{2P}")],
+            [MathTex(), MathTex(),MathTex(), MathTex("\ddots"), MathTex()],
+            [MathTex("M_{N1}"), MathTex("M_{N2}"),MathTex("M_{N3}"), MathTex("..."), MathTex("M_{NP}")]], h_buff = 1.6).shift(DOWN))
         m.append(MobjectMatrix([
             [MathTex("M_{11}"), MathTex("M_{12}"), MathTex("..."), MathTex("M_{1N}")],
             [MathTex("M_{21}"), MathTex("M_{22}"), MathTex("..."), MathTex("M_{2N}")],
             [MathTex(), MathTex(), MathTex("\ddots"), MathTex()],
-            [MathTex("M_{N1}"), MathTex("M_{N2}"), MathTex("..."), MathTex("M_{NN}")]], h_buff = 1.6))
+            [MathTex("M_{N1}"), MathTex("M_{N2}"), MathTex("..."), MathTex("M_{NN}")]], h_buff = 1.6).shift(DOWN))
         size = (5, 5)
         bounds = (-10, 10)
-        m.append(DecimalMatrix(np.round(np.random.uniform(*bounds,size=size), decimals=2), h_buff = 1.6))
-        m.append(RadomMatrix(size=size, bounds=bounds, precision=2, vt=vt))
+        m.append(DecimalMatrix(np.round(np.random.uniform(*bounds,size=size), decimals=2), h_buff = 1.6).shift(DOWN))
+        m.append(RadomMatrix(size=size, bounds=bounds, precision=2, vt=vt).shift(DOWN))
         
         # scale matricies then add first matrix to the scene
         for x in m:
             x.scale(0.5)
-        a = m[0].copy()
-        self.add(a)
+        m[0].scale(2)
+        a = first2b2
         self.wait(1)
-        
+        self.play(Write(a))
+        self.wait(1)
+        self.play(Transform(a,m[0]))
+        temp1=MobjectMatrix([[DecimalNumber(np.round(np.random.uniform(*bounds), decimals=2)) for y in range(2)] for x in range(2)], h_buff = 1.6)
+        temp2=MobjectMatrix([[DecimalNumber(np.round(np.random.uniform(*bounds), decimals=2)) for y in range(2)] for x in range(2)], h_buff = 1.6)
+        self.play(Transform(a,temp1))
+        self.play(Transform(a,temp2))
+        self.wait(1)
+        self.play(Transform(a,second2b2))
+        self.wait(1)
+         
         # # initialize axis and graphs for the two distributions
         axes1 = Axes(x_range=[-5,6,1], y_range=[0,1.1, 0.2]).scale(0.5).add_coordinates().scale(0.5).move_to(UL*2+LEFT*2)
         axes1.add(Tex("0").scale(0.4).next_to(axes1.x_axis.n2p(0), DOWN*0.5))
@@ -99,11 +116,15 @@ class RandomMatrix(Scene):
         self.wait(1)
 
         # # shows sizes of matrices 
-        b = Brace(m[6])
-        b1tex, b2tex = b.get_tex("N\\text{x}P"), b.get_tex("N\\text{x}N") 
-        self.play(Transform(a, m[6]),FadeIn(b,b1tex))
+        b1 = Brace(m[6])
+        b2 = Brace(m[7])
+        b1tex, b2tex = b1.get_tex("N\\text{x}P"), b2.get_tex("N\\text{x}N") 
+        self.play(Transform(a, m[6]),FadeIn(b1,b1tex))
         self.wait(1)
-        self.play(Transform(a, m[7]), Transform(b1tex, b2tex))
+        self.play(Transform(a, m[7]), Transform(b1tex, b2tex),ReplacementTransform(b1,b2))
+        self.wait(1)
+        self.play(Wiggle(element_types[0],scale_value=2))
+        self.play(Wiggle(element_types[1],scale_value=1.5))
         self.wait(1)
         
         # # show specific case of matrix 5x5 (-10,10)
@@ -116,7 +137,7 @@ class RandomMatrix(Scene):
         element_types.remove(integer_tex)
         self.play(
             Transform(a, m[8]),
-            FadeOut(guas,guas_area,b,b1tex),
+            FadeOut(guas,guas_area,b2,b1tex),
             ReplacementTransform(uniform,uniformN),
             ReplacementTransform(uniform_area,uniform_areaN),
             FadeIn(axes3),
@@ -139,18 +160,18 @@ class RandomMatrix(Scene):
         self.play(vt.animate.set_value(0.3), ReplacementTransform(integer_arrow,left_arrow), run_time=2)
         self.play(vt.animate.set_value(0.6), ReplacementTransform(left_arrow,right_arrow), run_time=2)
         self.play(vt.animate.set_value(1), ReplacementTransform(right_arrow,integer_arrow_copy), run_time=2)
-        self.play(FadeOut(integer_arrow_copy))
+        self.play(FadeOut(integer_arrow_copy,entry_equals_tex,integer_texN))
 
         # show creation of 3 matricies then puts into row
         self.remove(a) 
         m_group = VGroup()
         original_vals = np.round(np.random.uniform(*bounds,size=(3,*size)), decimals=2)
         for i in range(3):
-            temp = DecimalMatrix(original_vals[i], h_buff = 1.6).scale(0.5)
+            temp = DecimalMatrix(original_vals[i], h_buff = 1.6).shift(DOWN).scale(0.5)
             self.add(temp)
             self.wait(0.25)
             self.play(temp.animate.scale(0.4).move_to(LEFT * 2*(1-i) + DOWN * 1.5))
-            self.wait(0.25)
+            self.wait(0.25) 
             m_group.add(temp)
 
         # show ensemble as collection of 3 previously created matricies
@@ -158,31 +179,46 @@ class RandomMatrix(Scene):
         left_brace, right_brace = MathTex("\\{").scale(1.7), MathTex("\\}").scale(1.7)
         ensemble = VGroup(MathTex("ensemble: "), left_brace, m_sub[0].copy(), m_sub[1].copy(), MathTex("..."), m_sub[2].copy(), right_brace).arrange(RIGHT)
         self.play(TransformMatchingShapes(m_group, ensemble)) # tries tranforming all submobjects to ones in other group that match??? tranforming matching shapes does not work like normal transfrom method
-        self.wait(1)
+        
 
         # transform ensemble into single decimal matrix
-        a = DecimalMatrix(original_vals[0], h_buff = 1.6).scale(0.5)
+        a = DecimalMatrix(original_vals[0], h_buff = 1.6).shift(DOWN).scale(0.5)
         self.play(TransformMatchingShapes(ensemble,a))
         self.wait(1)
         
+        # iid explanation
+        prev_arrow = Mobject()
+        for i in [5,13,19]:
+            curr_arrow = CurvedArrow(a.get_entries()[i].get_center(),axes3.c2p(a.get_entries()[i].get_value(),0.8),color=YELLOW)
+            self.play(Create(curr_arrow),FadeOut(prev_arrow))
+            prev_arrow = curr_arrow
+        self.play(FadeOut(prev_arrow))
+        iid_tex = Tex("iid = Independent and identically").next_to(axes3).shift(UP*0.5)
+        iid_tex.add(Tex("distributed random variables").next_to(iid_tex,DOWN))
+        self.play(Write(iid_tex))
+        self.wait(1)
+        self.play(FadeOut(iid_tex))
+        self.wait(1)
 
-        #transform matrix into list on entries then back
+        # transform matrix into list on entries then back
         entries = a.copy().get_entries()
         left_brace, right_brace = MathTex("["), MathTex("]")
         decimal_list = VGroup(*[left_brace,*entries[:3], MathTex("..."), *entries[-2:],right_brace]).arrange(RIGHT)
         self.play(TransformMatchingShapes(a,decimal_list))
         self.wait(1)
         self.play(TransformMatchingShapes(decimal_list,a))
+        self.wait(1)
         
         #cycle through entries and show that they are drawn independently from the distribution 
         independent_tex1 = MathTex("p(M)","=","p(M_{11})p(M_{12})...p(M_{NN})").scale(0.5).next_to(a,RIGHT)
         independent_tex2 = MathTex("p(M)","=","\prod_{ij=1}^{N} M_ {ij}").scale(0.5).next_to(a,RIGHT)
         dependent_tex = MathTex("p(M)","\\neq","\prod_{ij=1}^{N} M_{ij}").scale(0.5).next_to(a,RIGHT)
-        symetric_tex =  MathTex("p(M)","=","\prod_{indep ij} M_{ij} = \prod_{i} M_{ii}\prod_{i>j} M_{ij}").scale(0.5).next_to(a,RIGHT)
+        symetric_tex =  MathTex("p(M)","=","\prod_{i,j} M_{ij} = \prod_{i} M_{ii}\prod_{i>j} M_{ij}").scale(0.5).next_to(a,RIGHT)
+        
         prev_arrow = Mobject()
         for i in range(25):
             curr_arrow = CurvedArrow(a.get_entries()[i].get_center(),axes3.c2p(a.get_entries()[i].get_value(),0.8),color=YELLOW)
-            self.play(
+            self.play( 
                 ShowPassingFlash(SurroundingRectangle(a.get_entries()[i]),time_width=1),
                 FadeOut(prev_arrow),
                 Create(curr_arrow), 
@@ -195,39 +231,56 @@ class RandomMatrix(Scene):
         self.play(FadeOut(prev_arrow))
 
         # show physical system is not independent
-        ps = Tex("PYS SYSTM").move_to(LEFT*3)
-        self.play(FadeIn(ps),run_time=0.5)
+        #ps = Tex("PYS SYSTM").move_to(LEFT*3)
+        #self.play(FadeIn(ps),run_time=0.5)
         self.play(TransformMatchingTex(independent_tex2,dependent_tex))
-        self.play(Indicate(dependent_tex.get_part_by_tex("\\neq"), scale_factor=3, color=RED),FadeOut(entry_equals_tex,integer_texN))
+        self.play(Indicate(dependent_tex.get_part_by_tex("\\neq"), scale_factor=3, color=RED))
         self.wait(1)
-        self.play(FadeOut(ps),run_time=0.5)
+        #self.play(FadeOut(ps),run_time=0.5)
 
         # show defenition of symetric matrix with two matching entries
         entries = [a.get_entries()[i:i+5] for i in range(0,25,5)]
         rect1_start = SurroundingRectangle(entries[0][0])
-        rect1_middle = SurroundingRectangle(entries[0][3])
+        rect1_middle = SurroundingRectangle(entries[1][0]) 
         rect1_end = SurroundingRectangle(entries[1][3])
 
         rect2_start = SurroundingRectangle(entries[0][0])
         rect2_middle = SurroundingRectangle(entries[3][0])
         rect2_end = SurroundingRectangle(entries[3][1])
         
-        self.play(Create(rect1_start))
-        self.play(ReplacementTransform(rect1_start,rect1_middle))
-        self.play(ReplacementTransform(rect1_middle,rect1_end))
-
         self.play(Create(rect2_start))
         self.play(ReplacementTransform(rect2_start,rect2_middle))
         self.play(ReplacementTransform(rect2_middle,rect2_end))
+        
+        self.play(Create(rect1_start))
+        self.play(ReplacementTransform(rect1_start,rect1_middle))
+        self.play(ReplacementTransform(rect1_middle,rect1_end))
 
         changed_num = entries[3][1].copy().set_value(entries[1][3].get_value())
         self.play(Transform(entries[3][1],changed_num),Transform(rect2_end,SurroundingRectangle(changed_num)))
         self.wait(1)
         
         # transform into fully systmetric matrix
-        sysmetric_matrix = DecimalMatrix(np.tril(original_vals[0].T) + np.triu(original_vals[0], 1)).scale(0.5)
+        sysmetric_matrix = DecimalMatrix(np.tril(original_vals[0].T) + np.triu(original_vals[0], 1)).shift(DOWN).scale(0.5)
         self.play(ReplacementTransform(a,sysmetric_matrix),TransformMatchingTex(dependent_tex,symetric_tex),FadeOut(rect1_end,rect2_end))
         self.wait(1)
+
+        # pick out half of the entries
+        n2 = len(sysmetric_matrix.get_entries())
+        n = int(n2**(1/2))
+        entries = [sysmetric_matrix.get_entries()[i:i+n] for i in range(0,n2,n)]
+        wiggle_list = []
+        for i in range(n):
+            for j in range(n):
+                if i>=j:
+                    wiggle_list.append(Wiggle(entries[j][i]))
+        self.play(*wiggle_list)
+        
+        # N*(N+1)/2
+        count_tex = MathTex("\\frac{N*(N+1)}{2}").next_to(axes3,RIGHT)
+        self.play(Write(count_tex))
+        self.wait(1)
+        
 
         # fadeout symetric entry values (lower triangular section)
         sysmetric_matrix_faded = fade_lower_triangle(sysmetric_matrix.copy())
@@ -235,9 +288,9 @@ class RandomMatrix(Scene):
         self.wait(1)
 
         # show vector of random variables and covariance between then in matrix
-        x_list = MathTex("X"," = ","(","x_{1}",",","x_{2}",",","x_{3}",",","...",",","x_{N}",")").next_to(a,UP).shift(UP)
-        C_def = MathTex("C_{ij}(X) "," \equiv ","cov(x_i,x_j)").next_to(x_list,DOWN)
-        self.play(Write(x_list),FadeOut(axes3,uniform_areaN,uniformN))
+        x_list = MathTex("X"," = ","(","x_{1}",",","x_{2}",",","x_{3}",",","...",",","x_{N}",")").next_to(a.copy().shift(UP),UP).shift(UP)
+        C_def = MathTex("C_{ij}(X) "," \equiv ","\\text{cov}(x_i,x_j)").next_to(x_list,DOWN)
+        self.play(Write(x_list),FadeOut(axes3,uniform_areaN,uniformN,count_tex))
         self.wait(1)
         cov_matrix = MobjectMatrix([
             [MathTex("C(x_{1},x_{1})"), MathTex("C(x_{1},x_{2})"), MathTex("C(x_{1},x_{3})"), MathTex("..."), MathTex("C(x_{1},x_{N})")],
@@ -265,7 +318,7 @@ class RandomMatrix(Scene):
         self.wait(1)
 
         # write equivalency tex 
-        equivalent1 = MathTex("cov(a,b) = cov(b,a)").next_to(a,DOWN)
+        equivalent1 = MathTex("\\text{cov}(a,b) = \\text{cov}(b,a)").next_to(a,DOWN)
         equivalent2 = MathTex("X_{ij} = X_{ji}").next_to(equivalent1,DOWN)
         self.play(Write(equivalent1))
         self.wait(0.5)
@@ -277,10 +330,10 @@ class RandomMatrix(Scene):
         self.wait(1)
 
         # change to quantum matrix with states
-        H_def = VGroup(MathTex("H_{ij}"),Tex("\\textasciitilde"),VGroup(Tex("Encodes probablility to"),Tex("transition from state"),Tex("i $\\leftarrow$ j in a small time j}")).arrange(DOWN)).arrange(RIGHT).move_to(x_list).shift(LEFT*2)
+        H_def = VGroup(MathTex("H_{ij}"),Tex("\\textasciitilde"),VGroup(Tex("Encodes probablility to"),Tex("transition from state"),Tex("i $\\rightarrow$ j in a small time $\\Delta$t.")).arrange(DOWN)).arrange(RIGHT).move_to(x_list).shift(LEFT*2)
         ham_matrix = MobjectMatrix([
             [MathTex("(1 -> 1"), MathTex("(1 -> 2)"), MathTex("(1 -> 3)"), MathTex("...   "), MathTex("(1 -> N)")],
-            [MathTex("(2 -> 1)"), MathTex("(2 -> 2)"),MathTex("(2 -> 3)"), MathTex("...  "), MathTex("(2 -> N)")],
+            [MathTex("(2 -> 1)"), MathTex("(2 -> 2)") ,MathTex("(2 -> 3)"), MathTex("...  "), MathTex("(2 -> N)")],
             [MathTex("(3 -> 1)"), MathTex("(3 -> 2)"), MathTex("(3 -> 3)"), MathTex("...  "), MathTex("(3 -> N)")],
             [MathTex(), MathTex(), MathTex(), MathTex("\ddots  "), MathTex()],
             [MathTex("(N -> 1)"), MathTex("(N -> 2)"), MathTex("(N -> 3)"), MathTex("...  "), MathTex("(N -> N)")]
@@ -298,5 +351,13 @@ class RandomMatrix(Scene):
         symetric_conj = [[str(int(array1[x][y]))+("+"*(int(array2[x][y])>=0)+str(int(array2[x][y]))+"i")*(x!=y) for y in range(size[1])] for x in range(size[0])]
         complex_symetric_matrix = Matrix(symetric_conj, h_buff = 2.6).scale(0.5)
         self.play(ReplacementTransform(faded_ham_matrix,complex_symetric_matrix),ReplacementTransform(equivalent2,equivalent_Complex))
+        self.wait(1)
+        self.play(Wiggle(equivalent_Complex))
+        self.wait(1)
+        self.play(Wiggle(complex_symetric_matrix.get_entries()[2],1.5))
+        self.wait(1)
+        self.play(Wiggle(complex_symetric_matrix.get_entries()[10],1.5))
+        self.wait(1)
+        self.play(*[Wiggle(complex_symetric_matrix.get_entries()[i],1.5) for i in range(0,25,6)])
         self.wait(1)
         
